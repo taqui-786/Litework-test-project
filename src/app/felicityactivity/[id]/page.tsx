d0e2259136adbf057016d4686cf80d23c5560f80
taqui-786 <mdtaqui.jhar@gmail.com>
2024-10-07 23:25:25 +0530

    Updated & released

--- src/app/felicityactivity/[id]/page.tsx
+++ src/app/felicityactivity/[id]/page.tsx
@@ -1,13 +1,21 @@
 import { FelicityActivityPage } from "@/components/FelicityactivityComponent/FelicityActivityPage";
 import React from "react";
+import type { Metadata } from 'next'
 
 interface PageProps {
-    params: { id: string };
+  params: { id: string };
+}
+
+// Change metadata to a function to accept the `params`
+export async function generateMetadata({ params: { id } }: PageProps): Promise<Metadata> {
+  return {
+    title: `Felicity | Activity ${id}`, 
+    description: `Details for activity ${id}`, 
+  };
 }
 
 function Page({ params: { id } }: PageProps) {
-
-    return <FelicityActivityPage />;
+  return <FelicityActivityPage />;
 }
 
 export default Page;
